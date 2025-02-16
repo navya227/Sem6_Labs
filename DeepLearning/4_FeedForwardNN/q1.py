a9c49fedb4102512277c9d8d0bc315012434fd6f
Navya Kulhari <123367288+navya227@users.noreply.github.com>
2025-02-16 16:32:46 +0530

    Update q1.py

--- DeepLearning/4_FeedForwardNN/q1.py
+++ DeepLearning/4_FeedForwardNN/q1.py
@@ -1,64 +1,72 @@
 import torch
-from torch.utils.data import DataLoader, Dataset
-import torch.nn as nn
 import matplotlib.pyplot as plt
-torch.manual_seed(0)
-
-class XORproblem(nn.Module):
-    def __init__(self):
-        super(XORproblem, self).__init__()
-        self.linear1 = nn.Linear(2, 2, bias=True)
-        self.activation1 = nn.Sigmoid()
-        self.linear2 = nn.Linear(2, 1, bias=True)
-
-    def forward(self, x):  
-        x = self.linear1(x)
-        x = self.activation1(x)
-        x = self.linear2(x)
-        return x
-
-class MyDataset(Dataset):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __len__(self):
-        return len(self.x)
-
-    def __getitem__(self, index):
-        return self.x[index].to(device), self.y[index].to(device)
-
-def criterion(model, train_data_loader, optimizer, loss_fn):
-    total_loss = 0
-    for data in train_data_loader:
-        input, labels = data
-        optimizer.zero_grad()
-        output = model(input)
-        loss = loss_fn(output.flatten(), labels) 
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-    return total_loss / len(train_data_loader)
-
-X = torch.tensor([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=torch.float32)
-Y = torch.tensor([0, 1, 1, 0], dtype=torch.float32)
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader,Dataset
 
 loss_list = []
-full_dataset = MyDataset(X, Y)
-batch_size = 1
-train_data_loader = DataLoader(full_dataset, batch_size=batch_size, shuffle=True)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
+torch.manual_seed(42)
 
-model = XORproblem().to(device)
-loss_fn = torch.nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.03)
-epoch = 1000
+x = torch.tensor([[0,0],[0,1],[1,0],[1,1]], dtype=torch.float32)
+y = torch.tensor([0,1,1,0], dtype=torch.float32).view(-1,1)
+lr = 0.001
 
-for iter in range(epoch):
-    model.train()
-    avg_loss = criterion(model, train_data_loader, optimizer, loss_fn)
-    loss_list.append(avg_loss)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+class myData (Dataset):
+    def __init__(self,X,Y):
+        self.X = X
+        self.Y = Y
+    def __len__(self):
+        return len(self.X)
+    def __getitem__(self, idx):
+        return self.X[idx],self.Y[idx]
 
-plt.plot(loss_list)
+dataset = myData(x,y)
+dataloader = DataLoader(dataset,batch_size=1,shuffle=True)
+
+class XORModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.l1 = nn.Linear(2,2,bias=True)
+        self.a1 = nn.Sigmoid()
+        self.l2 = nn.Linear(2,1,bias=True)
+
+    def forward(self,x):
+        x = self.l1(x)
+        x = self.a1(x)
+        x = self.l2(x)
+        return x
+
+model = XORModel().to(device)
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(),lr = lr)
+
+epochs = 1000
+loss_list = []
+
+for epoch in range(epochs):
+    l = 0.0
+    for i,(ip,tar) in enumerate(dataloader):
+        yp = model(ip)
+        loss = criterion(yp,tar)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        l+=loss.item()
+    l/=len(x)
+    loss_list.append(l)
+
+model.eval()
+with torch.no_grad():
+    for i, (inputs, labels) in enumerate(dataloader):
+        outputs = model(inputs)
+        print(inputs, outputs)
+
+for param in model.named_parameters():
+    print(param)
+
+total_params = sum(p.numel() for p in model.parameters())
+print(f"Total number of parameters: {total_params}")
+
+plt.plot(list(range(epochs)), loss_list)
 plt.show()
