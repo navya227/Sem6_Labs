e9a26ec1752574aa291d9a4c4bb3910f6c8148a6
Navya Kulhari <123367288+navya227@users.noreply.github.com>
2025-02-16 15:18:34 +0530

    Update q3.py

--- DeepLearning/3_Regression/q3.py
+++ DeepLearning/3_Regression/q3.py
@@ -1,5 +1,4 @@
 import torch
-import matplotlib.pyplot as plt
 
 x = torch.tensor([5.0, 7.0, 12.0, 16.0, 20.0])
 y = torch.tensor([40.0, 120.0, 180.0, 210.0, 240.0])
@@ -7,35 +6,34 @@ lr = torch.tensor(0.001)
 
 class RegressionModel:
     def __init__(self):
-        self.w = torch.rand([1], requires_grad=True)
-        self.b = torch.rand([1], requires_grad=True)
+        self.w = torch.tensor(1.0,requires_grad=True)
+        self.b = torch.tensor(1.0, requires_grad=True)
 
-    def forward(self, x):
-        return self.w*x+ self.b
+    def forward(self,x):
+        return x * self.w + self.b
 
     def update(self):
-        self.w -= lr*self.w.grad
-        self.b -= lr*self.w.grad
+        self.w -= lr * self.w.grad
+        self.b -= lr * self.b.grad
 
     def reset_grad(self):
         self.w.grad.zero_()
         self.b.grad.zero_()
 
-def criterion(yj,y_p):
-    return torch.mean((yj - y_p)**2)
+    def criterion(self,yp,y):
+        return (yp-y)**2
 
 model = RegressionModel()
-
 loss_list = []
-for epochs in range(100):
-    loss = 0
-    for j in range(len(x)):
-        y_p = model.forward(x[j])
-        loss += criterion(y[j],y_p)
 
-    loss = loss/len(x)
-    loss_list.append(loss.item())
+for epoch in range(100):
+    loss = 0.0
+    for i in range(len(x)):
+        yp = model.forward(x[i])
+        loss += model.criterion(yp,y[i])
 
+    loss/= len(x)
+    loss_list.append(loss)
     loss.backward()
 
     with torch.no_grad():
@@ -43,5 +41,6 @@ for epochs in range(100):
 
     model.reset_grad()
 
-plt.plot(loss_list)
-plt.show()
+print(model.w.item())
+print(model.b.item())
+print(loss.item())
