752bda51f8fce3114979649375f8678de47a07ad
Navya Kulhari <123367288+navya227@users.noreply.github.com>
2025-02-16 15:33:56 +0530

    Update q7.py

--- DeepLearning/3_Regression/q7.py
+++ DeepLearning/3_Regression/q7.py
@@ -1,53 +1,65 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
 
-X = torch.tensor([1, 5, 10, 10, 25, 50, 70, 75, 100]).view(-1, 1).float()  # Features
-y = torch.tensor([0, 0, 0, 0, 0, 1, 1, 1, 1]).view(-1, 1).float()  # Target labels
+x = torch.tensor( [1., 5., 10., 10., 25., 50., 70., 75., 100.]).view(-1, 1)
+y = torch.tensor( [0., 0., 0., 0., 0., 1., 1., 1., 1]).view(-1, 1)
+
+lr = 0.001
+epochs = 100
+
+class RegressionDataset(Dataset):
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.Y[idx]
+
+dataset = RegressionDataset(x, y)
+dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
 
 class LogisticRegression(nn.Module):
     def __init__(self):
-        super(LogisticRegression, self).__init__()
+        super().__init__()
         self.linear = nn.Linear(1, 1)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        return torch.sigmoid(self.linear(x))  # sigmoid activation
-
+        return self.sigmoid(self.linear(x))
 
 model = LogisticRegression()
 criterion = nn.BCELoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001)
+optimizer = optim.SGD(model.parameters(), lr=lr)
+
 loss_list = []
 
-# Training loop
-epochs = 1000
 for epoch in range(epochs):
-    model.train()
+    l = 0.0
+    for i, (ip, tar) in enumerate(dataloader):
+        yp = model(ip)
+        loss = criterion(yp, tar)
 
-    y_pred = model(X)
-    loss = criterion(y_pred, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        l += loss.item()
+
+    l /= len(dataloader)
+    loss_list.append(l)
 
 
-    loss_list.append(loss.item())
-
-plt.plot(loss_list)
+plt.plot(range(epochs), loss_list)
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
-plt.title('Training Loss')
+plt.title('Loss over Epochs')
 plt.show()
 
-with torch.no_grad():
-    y_pred_final = model(X)
-
-
-print("Learned weight (w):", model.linear.weight.item())
-print("Learned bias (b):", model.linear.bias.item())
-
-test_input = torch.tensor([[30.0]]).float()
-predicted_prob = model(test_input).item()
-print(f"Predicted probability for input 30: {predicted_prob:.4f}")
+print(f"Final W (Weight): {model.linear.weight.item()}")
+print(f"Final B (Bias): {model.linear.bias.item()}")
